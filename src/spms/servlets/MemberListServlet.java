f05ab0e7e9fd93a03512c3b3273e5f82263cf511
이원준 <cloudysoul@naver.com>
2016-02-14 00:54:47 +0900

    JDBC를 이용하여 회원정보 테이블의 데이터를 발췌하여 회원목록을 간단한 HTML로 출력하는 MemberListServlet 추가

---
+++ src/spms/servlets/MemberListServlet.java
@@ -0,0 +1,58 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/member/list")
+public class MemberListServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try{
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/studydb", "study", "study");
+			pstmt = conn.prepareStatement("select mno, email, mname, cre_date from members order by mno asc");
+			rs = pstmt.executeQuery();
+			
+			PrintWriter writer = response.getWriter();
+			writer.println("<html>");
+			writer.println("<head>");
+			writer.println("<title>회원 목록</title>");
+			writer.println("</head>");
+			writer.println("<body>");
+			writer.println("<h1>회원 목록</h1>");
+			while(rs.next()){
+				writer.println(rs.getInt("mno") + ", " + rs.getString("mname") + ", " + rs.getString("email") + ", " + rs.getString("cre_date") + "<br>");
+			}
+			writer.println("</body>");
+			writer.println("</html>");
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{ if(rs != null){ rs.close();} }catch(Exception e){}
+			try{ if(pstmt != null){ pstmt.close();} }catch(Exception e){}
+			try{ if(conn != null){ conn.close();} }catch(Exception e){}
+		}
+	}
+
+}
