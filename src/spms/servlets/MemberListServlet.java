897e6e2ff338fb62f5b40a619001949b9b5dd44e
이원준 <cloudysoul@naver.com>
2016-02-22 00:18:47 +0900

    MemberListServlet에서 직접 출력로직 제거하고 VO에 데이터를 담아서 MemberList.jsp 로 출력하도록 수정

--- src/spms/servlets/MemberListServlet.java
+++ src/spms/servlets/MemberListServlet.java
@@ -6,7 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -15,6 +17,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import spms.vo.Member;
 
 @WebServlet("/member/list")
 public class MemberListServlet extends HttpServlet {
@@ -38,21 +42,30 @@ public class MemberListServlet extends HttpServlet {
 			pstmt = conn.prepareStatement("select mno, email, mname, cre_date from members order by mno asc");
 			rs = pstmt.executeQuery();
 					
-			PrintWriter writer = response.getWriter();
-			writer.println("<html>");
-			writer.println("<head>");
-			writer.println("<title>회원 목록</title>");
-			writer.println("</head>");
-			writer.println("<body>");
-			writer.println("<h1>회원 목록</h1>");
-			writer.println("<p><a href='add'>신규 회원</a></p>");
+//			PrintWriter writer = response.getWriter();
+//			writer.println("<html>");
+//			writer.println("<head>");
+//			writer.println("<title>회원 목록</title>");
+//			writer.println("</head>");
+//			writer.println("<body>");
+//			writer.println("<h1>회원 목록</h1>");
+//			writer.println("<p><a href='add'>신규 회원</a></p>");
+//			while(rs.next()){
+//				mno = rs.getInt("mno");
+//				writer.println(mno + ", <a href='update?no=" + mno + "' method='GET'>" + rs.getString("mname") + "</a>, " + rs.getString("email") + ", " + rs.getString("cre_date") + 
+//						" <a href='delete?no=" + mno + "' method='GET'>[삭제]</a><br>");
+//			}
+//			writer.println("</body>");
+//			writer.println("</html>");
+			
+			ArrayList<Member> members = new ArrayList<Member>();
 			while(rs.next()){
-				mno = rs.getInt("mno");
-				writer.println(mno + ", <a href='update?no=" + mno + "' method='GET'>" + rs.getString("mname") + "</a>, " + rs.getString("email") + ", " + rs.getString("cre_date") + 
-						" <a href='delete?no=" + mno + "' method='GET'>[삭제]</a><br>");
+				members.add(new Member().setNo(rs.getInt("mno")).setName(rs.getString("mname")).setEmail(rs.getString("email")).setCreateDate(rs.getString("cre_date")));
 			}
-			writer.println("</body>");
-			writer.println("</html>");
+			request.setAttribute("members", members);
+			
+			RequestDispatcher  rd = request.getRequestDispatcher("/member/MemberList.jsp");
+			rd.include(request, response);
 			
 		}catch(Exception e){
 			e.printStackTrace();
